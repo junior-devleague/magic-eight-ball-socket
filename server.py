825845bc0b52cca207a323cdd8ac0f9ea722b005
Jay Won <jason@sudokrew.com>
2016-06-12 01:45:56 -1000

    Add random answers and continuation of game

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import socket #import socket Python library, socket is a single class
 import sys #import OS related Python library, sys is a single class
 from thread import *  #import all functions from the thread library by their own name
+import random #import random Python library, random is a single class
 
 HOST = '' #What is the HOST?
 PORT = 8888 #What is the PORT?
+
+#Create array of answers for questions
+MAGIC_ANSWERS = ['Not likely.', 'As if.', 'Yup!', 'You Betcha!', 'You Must Wait For The Answer..', 'Nick Jonas says it is so.']
 
 #Open a new IP socket for streaming
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -33,14 +37,25 @@ def clientthread(conn):
 
         #Receive new messages from the client
         data = conn.recv(1024)
+
+
+        #Make a new variable with a String to repeat what the client asked
         reply = 'You asked: ' + data
+
+        #If they didn't ask a question don't continue any farther
         if not data:
             break
 
+        #Repeat the question to the user that they asked
+        conn.sendall(reply)
+
+        reply = 'The Magic Eight Ball says: ' + random.choice(MAGIC_ANSWERS) + '\n'
+        conn.sendall(reply)
+
+        reply = 'What is your next question?\n'
         conn.sendall(reply)
 
     conn.close() #close only this connection
-        
 
 
 while 1:
@@ -48,6 +63,7 @@ while 1:
     conn, addr = s.accept()
     print 'Connected to by: ' + addr[0] + ':' + str(addr[1])
 
+    #Handoff the new connection to a new thread and call our clientthread function
     start_new_thread(clientthread, (conn,))
 
 s.close() #close the connection
